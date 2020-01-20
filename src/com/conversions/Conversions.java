5f1c64ba7cde6c81ee94a028285249b9b008807f
Maxwell <maxwell.shiundlana@gmail.com>
2020-01-20 22:10:07 +0200

    Kelvin to celcius

--- src/com/conversions/Conversions.java
+++ src/com/conversions/Conversions.java
@@ -1,5 +1,19 @@
 package com.conversions;
 
 public class Conversions {
+    //kelvin to celcius
+    public String ktoc(float kelvin){
+            String output = "";
+            try{
+                    double celcius = kelvin - 273.15f;
+
+                    output = String.format("%.2fK is %.2f Degrees Celcius", kelvin, celcius);
+            }
+            catch (Exception e){
+            	output = String.format("An error has occured %s", e.getMessage());
+            }
+
+            return output;
+    }
 
 }
