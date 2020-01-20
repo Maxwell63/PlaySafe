aad977fd326bb6d0f228fde787e1f2405f3631b9
Maxwell <maxwell.shiundlana@gmail.com>
2020-01-20 23:10:03 +0200

    removed Barabara from print

--- ConsoleRoulette/src/com/consoleroulette/Main.java
+++ ConsoleRoulette/src/com/consoleroulette/Main.java
@@ -34,7 +34,7 @@ public class Main {
     }    
     
     public static void winnings(){  
-        System.out.println("Player\t\tBarbaraTotal Win\tTotal Bet"); 
+        System.out.println("Player\t\tTotal Win\tTotal Bet"); 
         System.out.println(String.format("---"));
                
         for (Map.Entry<String, Player> entry : players.entrySet()) {
